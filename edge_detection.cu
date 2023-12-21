b6f4382daa59ffcf2eae0eb783cf41cbf0708adb
mnotin <94868539+mnotin@users.noreply.github.com>
2023-12-22 00:37:22 +0100

    Begin implementation to process blocks' borders

--- edge_detection.cu
+++ edge_detection.cu
@@ -9,27 +9,30 @@
  * Applies a gaussian blur over a matrix.
  **/
 void gaussian_blur(unsigned char *h_matrix, int matrix_width, int matrix_height) {
-  const int KERNEL_SIZE = 3;
-  float gaussian_blur_kernel[KERNEL_SIZE*KERNEL_SIZE] = {1/16.0, 2/16.0, 1/16.0, 
+  const int KERNEL_WIDTH = 3;
+  float gaussian_blur_kernel[KERNEL_WIDTH*KERNEL_WIDTH] = {1/16.0, 2/16.0, 1/16.0, 
                                                          2/16.0, 4/16.0, 2/16.0, 
                                                          1/16.0, 2/16.0, 1/16.0};
  
-  unsigned char *d_matrix;
+  unsigned char *d_input_matrix;
+  unsigned char *d_output_matrix;
   float *d_kernel;
-  cudaMalloc((void **) &d_matrix, matrix_width * matrix_height * sizeof(unsigned char));
-  cudaMalloc((void **) &d_kernel, KERNEL_SIZE*KERNEL_SIZE * sizeof(float));
+  cudaMalloc((void **) &d_input_matrix, matrix_width * matrix_height * sizeof(unsigned char));
+  cudaMalloc((void **) &d_output_matrix, matrix_width * matrix_height * sizeof(unsigned char));
+  cudaMalloc((void **) &d_kernel, KERNEL_WIDTH*KERNEL_WIDTH * sizeof(float));
 
-  cudaMemcpy(d_matrix, h_matrix, matrix_width*matrix_height*sizeof(unsigned char), cudaMemcpyHostToDevice);
-  cudaMemcpy(d_kernel, gaussian_blur_kernel, KERNEL_SIZE*KERNEL_SIZE * sizeof(int), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_input_matrix, h_matrix, matrix_width*matrix_height*sizeof(unsigned char), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_kernel, gaussian_blur_kernel, KERNEL_WIDTH*KERNEL_WIDTH * sizeof(int), cudaMemcpyHostToDevice);
 
   dim3 threads = dim3(MATRIX_SIZE_PER_BLOCK, MATRIX_SIZE_PER_BLOCK);
   dim3 blocks = dim3(matrix_width/MATRIX_SIZE_PER_BLOCK, matrix_height/MATRIX_SIZE_PER_BLOCK);
   printf("Nombre de blocs lancés: %d %d\n", blocks.x, blocks.y);
-  convolution<<<blocks, threads>>>(d_matrix, matrix_width, matrix_height, d_kernel, 3);
+  convolution<<<blocks, threads>>>(d_input_matrix, d_output_matrix, matrix_width, matrix_height, d_kernel, KERNEL_WIDTH);
  
-  cudaMemcpy(h_matrix, d_matrix, matrix_width*matrix_height*sizeof(unsigned char), cudaMemcpyDeviceToHost);
+  cudaMemcpy(h_matrix, d_output_matrix, matrix_width*matrix_height*sizeof(unsigned char), cudaMemcpyDeviceToHost);
 
-  cudaFree(d_matrix);
+  cudaFree(d_input_matrix);
+  cudaFree(d_output_matrix);
   cudaFree(d_kernel);
 }
 
@@ -46,15 +49,18 @@ void sobel_feldman(unsigned char *h_matrix, int matrix_width, int matrix_height)
                                                                   0,  0,  0,
                                                                  -1, -2, -1};
  
-  unsigned char *d_matrix;
+  unsigned char *d_input_matrix;
+  unsigned char *d_output_matrix;
   unsigned char *d_horizontal_edges;
   unsigned char *d_vertical_edges;
   float *d_kernel;
-  cudaMalloc((void **) &d_matrix, matrix_width * matrix_height * sizeof(unsigned char));
+  cudaMalloc((void **) &d_input_matrix, matrix_width * matrix_height * sizeof(unsigned char));
+  cudaMalloc((void **) &d_output_matrix, matrix_width * matrix_height * sizeof(unsigned char));
   cudaMalloc((void **) &d_horizontal_edges, matrix_width * matrix_height * sizeof(unsigned char));
   cudaMalloc((void **) &d_vertical_edges, matrix_width * matrix_height * sizeof(unsigned char));
   cudaMalloc((void **) &d_kernel, KERNEL_SIZE*KERNEL_SIZE * sizeof(float));
 
+  cudaMemcpy(d_input_matrix, h_matrix, matrix_width*matrix_height*sizeof(unsigned char), cudaMemcpyHostToDevice);
   cudaMemcpy(d_horizontal_edges, h_matrix, matrix_width*matrix_height*sizeof(unsigned char), cudaMemcpyHostToDevice);
   cudaMemcpy(d_vertical_edges, h_matrix, matrix_width*matrix_height*sizeof(unsigned char), cudaMemcpyHostToDevice);
 
@@ -62,15 +68,16 @@ void sobel_feldman(unsigned char *h_matrix, int matrix_width, int matrix_height)
   dim3 blocks = dim3(matrix_width/MATRIX_SIZE_PER_BLOCK, matrix_height/MATRIX_SIZE_PER_BLOCK);
   cudaMemcpy(d_kernel, sobel_kernel_horizontal_kernel, KERNEL_SIZE*KERNEL_SIZE * sizeof(int), cudaMemcpyHostToDevice);
   printf("Nombre de blocs lancés: %d %d\n", blocks.x, blocks.y);
-  convolution<<<blocks, threads>>>(d_horizontal_edges, matrix_width, matrix_height, d_kernel, 3);
+  convolution<<<blocks, threads>>>(d_input_matrix, d_horizontal_edges, matrix_width, matrix_height, d_kernel, 3);
   cudaMemcpy(d_kernel, sobel_kernel_vertical_kernel, KERNEL_SIZE*KERNEL_SIZE * sizeof(int), cudaMemcpyHostToDevice);
-  convolution<<<blocks, threads>>>(d_vertical_edges, matrix_width, matrix_height, d_kernel, 3);
-  // cudaDeviceSynchronize();
-  global_gradient<<<blocks, threads>>>(d_matrix, d_horizontal_edges, d_vertical_edges, matrix_width, matrix_height);
+  convolution<<<blocks, threads>>>(d_input_matrix, d_vertical_edges, matrix_width, matrix_height, d_kernel, 3);
+  cudaDeviceSynchronize();
+  global_gradient<<<blocks, threads>>>(d_output_matrix, d_horizontal_edges, d_vertical_edges, matrix_width, matrix_height);
  
-  cudaMemcpy(h_matrix, d_matrix, matrix_width*matrix_height*sizeof(unsigned char), cudaMemcpyDeviceToHost);
+  cudaMemcpy(h_matrix, d_output_matrix, matrix_width*matrix_height*sizeof(unsigned char), cudaMemcpyDeviceToHost);
 
-  cudaFree(d_matrix);
+  cudaFree(d_input_matrix);
+  cudaFree(d_output_matrix);
   cudaFree(d_horizontal_edges);
   cudaFree(d_vertical_edges);
   cudaFree(d_kernel);
--- main.cu
+++ main.cu
@@ -11,7 +11,7 @@
  * Applies discrete convolution over a matrix using a given kernel.
  * This kernel should be called using appropriate number of grids, blocks and threads to match the resolution of the image.
  **/
-__global__ void convolution(unsigned char *matrix, int matrix_width, int matrix_height, float *kernel, int kernel_size) {
+__global__ void convolution(unsigned char *input_matrix, unsigned char *output_matrix, int matrix_width, int matrix_height, float *kernel, int kernel_size) {
   int globalIdxX = threadIdx.x + (blockIdx.x * blockDim.x);
   int globalIdxY = threadIdx.y + (blockIdx.y * blockDim.y);
   int localIdxX = globalIdxX % MATRIX_SIZE_PER_BLOCK;
@@ -20,25 +20,47 @@ __global__ void convolution(unsigned char *matrix, int matrix_width, int matrix_
   int current_matrix_index = globalIdxY*matrix_width + globalIdxX;
   int current_shared_matrix_index = localIdxY*MATRIX_SIZE_PER_BLOCK + localIdxX;
 
-  __shared__ unsigned char shared_matrix[MATRIX_SIZE_PER_BLOCK*MATRIX_SIZE_PER_BLOCK];
-  shared_matrix[current_shared_matrix_index] = matrix[current_matrix_index];
+  __shared__ unsigned char shared_matrix[(MATRIX_SIZE_PER_BLOCK+2)*(MATRIX_SIZE_PER_BLOCK+2)];
+
+  /*
+   * x x x x x x MATRIX_SIZE_PER_BLOCK + 2
+   * x o o o o x
+   * x o o o o x
+   * x o o o o x
+   * x o o o o x
+   * x x x x x x
+   */
+  shared_matrix[MATRIX_SIZE_PER_BLOCK+2+1+2*(localIdxY) + current_shared_matrix_index] = input_matrix[current_matrix_index];
+  if (localIdxX == 0 && localIdxY == 0 && 0 < globalIdxX && globalIdxX < matrix_width-1 && 0 < globalIdxY && globalIdxY < matrix_height-1) {
+    for (int i = 0; i < MATRIX_SIZE_PER_BLOCK+2; i++) {
+     shared_matrix[i] = input_matrix[(globalIdxY-1)*matrix_width + globalIdxX + i - 1]; // First line
+     shared_matrix[(MATRIX_SIZE_PER_BLOCK+2)*(MATRIX_SIZE_PER_BLOCK+1)] = input_matrix[(globalIdxY+1)*matrix_width + globalIdxX + i - 1]; // Last line
+    }
+
+    for (int i = 0; i < MATRIX_SIZE_PER_BLOCK; i++) {
+     shared_matrix[MATRIX_SIZE_PER_BLOCK+2 + i*(MATRIX_SIZE_PER_BLOCK+2)] = input_matrix[globalIdxY*matrix_width + globalIdxX - 1]; // Left side
+     shared_matrix[MATRIX_SIZE_PER_BLOCK+2 + (i+1)*(MATRIX_SIZE_PER_BLOCK+2) - 1] = input_matrix[globalIdxY*matrix_width + globalIdxX + 1]; // Right side
+    }
+  }
   __syncthreads();
 
   float convolution_result = 0;
 
-  if (0 < localIdxX && localIdxX < MATRIX_SIZE_PER_BLOCK-1 && 0 < localIdxY && localIdxY < MATRIX_SIZE_PER_BLOCK-1) {
+  if (0 < globalIdxX && globalIdxX < matrix_width-1 && 0 < globalIdxY && globalIdxY < matrix_height-1) {
     for (int i = 0; i < kernel_size; i++) {
       for (int j = 0; j < kernel_size; j++) {
         int vertical_offset = ((localIdxY + i) - (int)floor(kernel_size/2.0));
         int horizontal_offset = (localIdxX + j) - (int)floor(kernel_size/2.0);
         int tmp_index = vertical_offset*MATRIX_SIZE_PER_BLOCK + horizontal_offset;
 
-        convolution_result += shared_matrix[tmp_index] * kernel[i*kernel_size + j];
+        convolution_result += shared_matrix[MATRIX_SIZE_PER_BLOCK+2+1+2*(localIdxY) + tmp_index] * kernel[i*kernel_size + j];
       }
     }  
+  } else {
+    // Matrix border
   }
   
-  matrix[current_matrix_index] = convolution_result;
+  output_matrix[current_matrix_index] = convolution_result;
 }
 
 int main(int argc, char **argv) {
