3228b5cd4dbc905e78db0838de512beb3a3026d5
mnotin <94868539+mnotin@users.noreply.github.com>
2024-01-24 22:08:35 +0100

    Gaussian blur: fix a bug related to the dimensions of the image (#1)

--- utils/gaussian_blur.cu
+++ utils/gaussian_blur.cu
@@ -10,9 +10,9 @@
  **/
 void ProcessingUnitDevice::gaussian_blur(unsigned char *h_matrix, dim3 matrix_dim) {
   const int KERNEL_WIDTH = 3;
-  float gaussian_blur_kernel[KERNEL_WIDTH*KERNEL_WIDTH] = {1/16.0, 2/16.0, 1/16.0, 
-                                                           2/16.0, 4/16.0, 2/16.0, 
-                                                           1/16.0, 2/16.0, 1/16.0};
+  const float GAUSSIAN_BLUR_KERNEL[KERNEL_WIDTH*KERNEL_WIDTH] = {1/16.0, 2/16.0, 1/16.0, 
+                                                                 2/16.0, 4/16.0, 2/16.0, 
+                                                                 1/16.0, 2/16.0, 1/16.0};
   int h_int_matrix[matrix_dim.x*matrix_dim.y];
  
   unsigned char *d_input_matrix;
@@ -23,10 +23,10 @@ void ProcessingUnitDevice::gaussian_blur(unsigned char *h_matrix, dim3 matrix_di
   cudaMalloc(&d_kernel, KERNEL_WIDTH*KERNEL_WIDTH * sizeof(float));
 
   cudaMemcpy(d_input_matrix, h_matrix, matrix_dim.x*matrix_dim.y*sizeof(unsigned char), cudaMemcpyHostToDevice);
-  cudaMemcpy(d_kernel, gaussian_blur_kernel, KERNEL_WIDTH*KERNEL_WIDTH * sizeof(int), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_kernel, GAUSSIAN_BLUR_KERNEL, KERNEL_WIDTH*KERNEL_WIDTH * sizeof(int), cudaMemcpyHostToDevice);
 
-  dim3 threads = dim3(MATRIX_SIZE_PER_BLOCK, MATRIX_SIZE_PER_BLOCK);
-  dim3 blocks = dim3(matrix_dim.x/MATRIX_SIZE_PER_BLOCK, matrix_dim.y/MATRIX_SIZE_PER_BLOCK);
+  dim3 threads(MATRIX_SIZE_PER_BLOCK, MATRIX_SIZE_PER_BLOCK);
+  dim3 blocks(ceil((float) matrix_dim.x/MATRIX_SIZE_PER_BLOCK), ceil((float) matrix_dim.y/MATRIX_SIZE_PER_BLOCK));
   std::cout << "Nombre de blocs lancés: " << blocks.x << " " << blocks.y << std::endl;
   convolution_kernel<<<blocks, threads>>>(d_input_matrix, d_output_matrix, matrix_dim, d_kernel, KERNEL_WIDTH);
  
