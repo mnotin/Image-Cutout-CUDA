b9c994af97812bf833f90dabe6f91fce036adf99
mnotin <94868539+mnotin@users.noreply.github.com>
2024-01-24 18:12:41 +0100

    RGB to gray: code refactoring and fix a bug related to the dimensions of the image (#1)

--- utils/rgb_to_gray.cu
+++ utils/rgb_to_gray.cu
@@ -4,59 +4,54 @@
 #include "../main.hpp"
 
 __global__ void rgb_to_gray_kernel(unsigned char *rgb_image, unsigned char *gray_image, dim3 image_dim) {
-  int2 index = make_int2(threadIdx.x + (blockIdx.x * blockDim.x), threadIdx.y + (blockIdx.y * blockDim.y));
+  int2 global_index = make_int2(threadIdx.x + (blockIdx.x * blockDim.x), threadIdx.y + (blockIdx.y * blockDim.y));
 
+  if (image_dim.x <= global_index.x || image_dim.y <= global_index.y) {
+    return;
+  }
 
-  gray_image[index.y*image_dim.x + index.x] = rgb_to_gray_core(index, rgb_image, image_dim);
+  gray_image[global_index.y*image_dim.x + global_index.x] = rgb_to_gray_core(global_index, rgb_image, image_dim);
 }
 
 __device__ __host__ unsigned char rgb_to_gray_core(int2 index, unsigned char *rgb_image, dim3 image_dim) {
   unsigned char r = 0, g = 0, b = 0;
 
-  if (index.y*image_dim.x+index.x < image_dim.x * image_dim.y) {
-    r = rgb_image[3 * (index.y*image_dim.x + index.x)];
-    g = rgb_image[3 * (index.y*image_dim.x + index.x) + 1];
-    b = rgb_image[3 * (index.y*image_dim.x + index.x) + 2];
-  }
+  r = rgb_image[3 * (index.y*image_dim.x + index.x)];
+  g = rgb_image[3 * (index.y*image_dim.x + index.x) + 1];
+  b = rgb_image[3 * (index.y*image_dim.x + index.x) + 2];
 
   return (0.21 * r + 0.71 * g + 0.07 * b);
 }
 
 void ProcessingUnitDevice::rgb_to_gray(RGBImage *h_rgb_image, GrayImage *h_gray_image) {
-  dim3 rgb_image_dim(h_rgb_image->width, h_rgb_image->height);
+  dim3 image_dim(h_rgb_image->width, h_rgb_image->height);
 
-  // Allocating device memory
   unsigned char *d_rgb_image;
   unsigned char *d_gray_image;
 
-  cudaMalloc(&d_rgb_image, sizeof(unsigned char) * (3 * h_rgb_image->width * h_rgb_image->height));
-  cudaMalloc(&d_gray_image, sizeof(unsigned char) * (h_gray_image->width * h_gray_image->height)); 
+  cudaMalloc(&d_rgb_image, sizeof(unsigned char) * (3 * image_dim.x * image_dim.y));
+  cudaMalloc(&d_gray_image, sizeof(unsigned char) * (image_dim.x * image_dim.y)); 
 
-  // Copying host memory to device
-  cudaMemcpy(d_rgb_image, h_rgb_image->data, 3 * h_rgb_image->width * h_rgb_image->height, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_rgb_image, h_rgb_image->data, 3 * image_dim.x * image_dim.y, cudaMemcpyHostToDevice);
 
-  // Initialize thread block and kernel grid dimensions
   dim3 threads = dim3(MATRIX_SIZE_PER_BLOCK, MATRIX_SIZE_PER_BLOCK);
-  dim3 blocks = dim3(h_rgb_image->width/MATRIX_SIZE_PER_BLOCK, h_rgb_image->height/MATRIX_SIZE_PER_BLOCK);
+  dim3 blocks = dim3(ceil((float) image_dim.x/MATRIX_SIZE_PER_BLOCK), ceil((float) image_dim.y/MATRIX_SIZE_PER_BLOCK));
 
-  // Invoke CUDA kernel
-  rgb_to_gray_kernel<<<blocks, threads>>>(d_rgb_image, d_gray_image, rgb_image_dim);
+  rgb_to_gray_kernel<<<blocks, threads>>>(d_rgb_image, d_gray_image, image_dim);
 
-  // Copy result from device to host
-  cudaMemcpy(h_gray_image->data, d_gray_image, h_gray_image->width * h_gray_image->height, cudaMemcpyDeviceToHost);
+  cudaMemcpy(h_gray_image->data, d_gray_image, image_dim.x * image_dim.y, cudaMemcpyDeviceToHost);
 
   cudaFree(d_rgb_image);
   cudaFree(d_gray_image);
 }
 
 void ProcessingUnitHost::rgb_to_gray(RGBImage *rgb_image, GrayImage *gray_image) {
-  dim3 gray_image_dim(gray_image->width, gray_image->height);
+  dim3 image_dim(rgb_image->width, rgb_image->height);
+  int2 index;
 
-  for (int i = 0; i < gray_image->height; i++) {
-    for (int j = 0; j < gray_image->width; j++) {
-      int2 index = make_int2(j, i);
-
-      gray_image->data[i*gray_image->width + j] = rgb_to_gray_core(index, rgb_image->data, gray_image_dim);
+  for (index.y = 0; index.y < gray_image->height; index.y++) {
+    for (index.x = 0; index.x < gray_image->width; index.x++) {
+      gray_image->data[index.y*image_dim.x + index.x] = rgb_to_gray_core(index, rgb_image->data, image_dim);
     }
   }
 }
