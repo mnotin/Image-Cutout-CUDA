23fd9cd3793f8b9487ee8bd71e2c24b5c2bcc869
mnotin <94868539+mnotin@users.noreply.github.com>
2024-02-05 23:58:19 +0100

    Remove tests.cu and tests.hpp

--- tests.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef TESTS_HPP
-#define TESTS_HPP
-
-#include "main.hpp"
-
-void test_canny(std::string filename, int2 cutout_start_pixel, int2 *tracking_start_pixel, int canny_min, int canny_max,
-                int canny_sample_offset, ProcessingUnit processing_unit, int file_index = 1);
-
-#endif // TESTS_HPP
